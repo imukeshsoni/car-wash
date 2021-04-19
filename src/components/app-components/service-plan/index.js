d335e148ff3fc098b6aab1244a29977cfc70c444
Mukesh Soni <msony552@gmail.com>
2021-04-19 18:10:04 +0530

    service component redesigned

--- src/components/app-components/service-plan/index.js
+++ src/components/app-components/service-plan/index.js
@@ -5,34 +5,30 @@ import { Button } from "../../base-components/button/index.js";
 
 function ServicePlan(props) {
   return (
-    <div className="service--container">
+    <div className="service--contents">
+      <img className="service--image" src={image} alt="not loaded" />
       <h2 className="service--plan--heading">
         {/* {props.heading} */}
         Service Heading
       </h2>
-
-      <div className="service--contents">
-        <img className="service--image" src={image} alt="not loaded" />
-        <div className="service--description">
-          <p className="">
-            {/* {props.details} */}
-            It is a long established fact that a reader will be distracted by
-            the readable content of a page when looking at its layout. The point
-            of using Lorem Ipsum is that it has a more-or-less normal
-            distribution of letters, as opposed to using 'Content here, content
-            here'.
-          </p>
-          <p className="service--amount">amount: 100</p>
-          {/* //TODO: Implement link for service view Select Plan */}
-          <Button
-            buttonStyle="btn--outline"
-            buttonSize="btn--largs"
-            linkTo="/"
-            key="0"
-          >
-            Select Plan
-          </Button>
-        </div>
+      <div className="service--description">
+        <p className="">
+          {/* {props.details} */}
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters, as opposed to using 'Content here, content here'.
+        </p>
+        <p className="service--amount">amount: 100</p>
+        {/* //TODO: Implement link for service view Select Plan */}
+        <Button
+          buttonStyle="btn--outline"
+          buttonSize="btn--largs"
+          linkTo="/"
+          key="0"
+        >
+          Select Plan
+        </Button>
       </div>
     </div>
   );
--- src/views/services/index.js
+++ src/views/services/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Footer from "../../components/app-components/footer/index.js";
 import ServicePlan from "../../components/app-components/service-plan/index.js";
+import "./styles.css";
 function Services() {
   return (
     <div>
-      <ServicePlan />
+      <div className="service--container">
+        <ServicePlan />
+        <ServicePlan />
+        <ServicePlan />
+      </div>
+
       <Footer />
     </div>
   );
