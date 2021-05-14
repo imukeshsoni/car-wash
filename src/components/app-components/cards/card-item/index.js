e17bfdc1a152a40123205d88311c07c2ceac107d
Mukesh Soni <msony552@gmail.com>
2021-05-14 15:50:12 +0530

    service plan rendered in cards section

--- src/components/app-components/cards/card-item/index.js
+++ src/components/app-components/cards/card-item/index.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function CardItem(props) {
-	return (
-			<div className='cards__item' onClick={props.onclick}>
-				<Link className='cards__item__link' to={props.path}>
-					<figure className='cards__item__pic-wrap' data-category={props.label}>
-						<img
-							className='cards__item__img'
-							alt='Travel Image'
-							src={props.src}
-						/>
-					</figure>
-					<div className='cards__item__info'>
-						<h5 className='cards__item__text'>{props.text}</h5>
-					</div>
-				</Link>
-			</div>
-	);
+  return (
+    <div className="cards__item" onClick={props.onclick}>
+      <Link className="cards__item__link" to={props.path}>
+        <figure className="cards__item__pic-wrap" data-category={props.label}>
+          <img
+            className="cards__item__img"
+            alt="Service plan"
+            src={props.src}
+          />
+        </figure>
+        <div className="cards__item__info">
+          <h5 className="cards__item__text">{props.text}</h5>
+          <h6 className="cards__item__text">
+            Price :{" "}
+            <span className="cards__item__amount"> ${props.amount}</span>
+          </h6>
+        </div>
+      </Link>
+    </div>
+  );
 }
 
 export default CardItem;
--- src/components/app-components/cards/index.js
+++ src/components/app-components/cards/index.js
@@ -1,50 +1,28 @@
 import React from "react";
 import CardItem from "./card-item/index.js";
 import "./styles.css";
-import image1 from "../../../assets/images/img-1.jpg";
+import image1 from "../../../assets/images/superfast.jpeg";
 import image2 from "../../../assets/images/img-2.jpg";
 import image3 from "../../../assets/images/img-home.jpg";
 function Cards() {
-  //TODO: get data from backend
-  const data = [
-    {
-      imagePath: image1,
-      title: "Explore superfast wash services at your doorstep!",
-      heading: "Superfast",
-      path: "/services",
-    },
-    {
-      imagePath: image2,
-      title: "Explore superfast wash services at your doorstep!",
-      heading: "Superfast",
-      path: "/services",
-    },
-    {
-      imagePath: image3,
-      title: "Explore superfast wash services at your doorstep!",
-      heading: "Superfast",
-      path: "/services",
-    },
-    {
-      imagePath: image3,
-      title: "Explore superfast wash services at your doorstep!",
-      heading: "Superfast",
-      path: "/services",
-    },
-  ];
+  const plans = JSON.parse(localStorage.getItem("plans"));
 
+  if (!plans) {
+    return "No plans available right now";
+  }
   return (
     <div className="cards">
       <h1 className="cards__heading">Check out these services for your car!</h1>
       <div className="cards__container">
-        {data.map((value, i) => {
+        {plans.map((value, i) => {
           return (
             <CardItem
               key={i}
               src={value.imagePath}
-              text={value.title}
-              label={value.heading}
-              path={value.path}
+              text={value.serviceDetails}
+              label={value.serviceName}
+              amount={value.serviceAmount}
+              path="/services"
             />
           );
         })}
