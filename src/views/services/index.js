6da0e8fc7241040fbb29395b85af6c5212ad3980
Mukesh Soni <msony552@gmail.com>
2021-05-14 16:05:13 +0530

    added plans in service page

--- src/views/services/index.js
+++ src/views/services/index.js
@@ -1,36 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import Footer from "../../components/app-components/footer/index.js";
 import "./styles.css";
 import { useSelector } from "react-redux";
 import { selectCars } from "../../redux/carSlice";
 
-const data = [
-  {
-    name: "SuperFast",
-    details: `Get your car Super fast, Super clean while you grab yourself a coffee.
-        Cleaning of dust and dirts on the body of the car will be taken care by us.`,
-    amount: "1000",
-  },
-  {
-    name: "Deep Clean",
-    details: `Get your car deeply cleaned under the hood.
-        Dust inside the seats of your car will be cleaned using vaccume.
-        Oiling will be done in neccessary parts.`,
-    amount: "3000",
-  },
-  {
-    name: "Mirror Clean",
-    details: `Your car will be cleaned deeply by removing all the dust and dirt inside out.
-        Special polishing will be done for the car body which creates a mirror effect when you look at the car.`,
-    amount: "5000",
-  },
-];
-export function clickHandler(i) {
-  console.log(data[i]);
-  return data[i];
-}
 function Services() {
   const user = JSON.parse(localStorage.getItem("user"));
+  const plans = JSON.parse(localStorage.getItem("plans"));
 
   let cars = useSelector(selectCars);
 
@@ -85,9 +61,14 @@ function Services() {
           <div className="custom--margin service--plans">
             <label htmlFor="paymentMode">Select Service Plan</label>
             <br />
-            <select name="services">
-              <option value="superfast">Superfast</option>
-              <option value="deepclean">Deep clean</option>
+            <select name="service">
+              {plans.map((value, i) => {
+                return (
+                  <option key={i} value={value.serviceId}>
+                    {value.serviceName}
+                  </option>
+                );
+              })}
             </select>
           </div>
 
