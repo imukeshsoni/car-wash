88ca56cafd00ed7dcc060fd7e4f2e38332ac0cd0
Mukesh Soni <msony552@gmail.com>
2021-05-16 17:59:23 +0530

    booking reducer is created

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,8 @@ import userReducer from "./userSlice.js";
 import carsReducer from "./carSlice.js";
 import orderReducer from "./orderSlice.js";
 import plansReducer from "./planSlice.js";
+import bookingReducer from "./bookingSlice.js";
+
 
 export default configureStore({
   reducer: {
@@ -10,5 +12,6 @@ export default configureStore({
     cars: carsReducer,
     orders: orderReducer,
     plans: plansReducer,
+    bookings: bookingReducer
   },
 });
